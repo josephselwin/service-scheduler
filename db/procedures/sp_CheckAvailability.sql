f4a0fdcdee938e2f161fbcecd5d639bddfa811fa
Joe Selwin <72773679+josephselwin@users.noreply.github.com>
2025-12-28 01:44:10 -0500

    Update availability check in sp_CheckAvailability.sql
    Commented out the previous appointment check and added a check for professional availability based on day of the week and time.

--- db/procedures/sp_CheckAvailability.sql
+++ db/procedures/sp_CheckAvailability.sql
@@ -7,14 +7,19 @@ BEGIN
     SET NOCOUNT ON;
 
     IF EXISTS (
-        SELECT 1
+       /* SELECT 1
         FROM Appointments
         WHERE ProfessionalId = @ProfessionalId
           AND Status IN ('Scheduled', 'InProgress')
           AND (
                 @StartDateTime < EndDateTime
             AND @EndDateTime > StartDateTime
-          )
+          ) */
+        SELECT 1
+    FROM ProfessionalAvailability PA
+    WHERE PA.ProfessionalId = @ProfessionalId
+      AND PA.DayOfWeek = DATEPART(WEEKDAY, @StartDateTime)
+      AND CAST(@StartDateTime AS TIME) BETWEEN PA.StartTime AND PA.EndTime
     )
         SELECT CAST(0 AS BIT) AS IsAvailable;
     ELSE
